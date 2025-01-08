740448466a29ab19167e909f6264bcda939c36dd
Sunnymaharshi <maharshireddythangasani@gmail.com>
2025-01-08 20:44:56 +0530

    notes added

---
+++ npm.js
@@ -0,0 +1,29 @@
+/*
+    npm 
+
+        npm init 
+            initializes project and creates package.json file
+        package versions
+            version format: major.minor.patch
+            ex: 8.9.23
+            patch version
+                for fixing bugs 
+            minor version
+                for adding features and still compatible with previous version
+            major version
+                not compatible features or changing major part of project
+        acceptable versions 
+            Exact version    
+                1.13.23
+            Greater than 
+                >1.13.23
+            Compatible changes
+                ^1.'13.23'
+            Minor changes 
+                ~1.13.'23'
+        dev dependency package
+            --save-dev or -D 
+        
+
+
+*/
